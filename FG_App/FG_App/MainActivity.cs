099ae940b29bd7d30900399166820b3ccb4af41a
Dargmuesli <jonas.thelemann@t-online.de>
2015-03-27 18:50:01 +0100

    Memory leak fix

--- FG_App/FG_App/MainActivity.cs
+++ FG_App/FG_App/MainActivity.cs
@@ -16,7 +16,7 @@ namespace FG_App
 {
 
 
-	[Activity (Label = "FG-App", MainLauncher = true, Icon = "@drawable/icon", Theme = "@style/CustomActionBarTheme")]
+	[Activity(Label = "FG-App", MainLauncher = true, Icon = "@drawable/icon", Theme = "@style/CustomActionBarTheme")]
 	public class MainActivity : Activity
 	{
 		DrawerLayout mDrawerLayout;
@@ -27,135 +27,132 @@ namespace FG_App
 
 
 
-		protected override void OnCreate (Bundle bundle)
+		protected override void OnCreate(Bundle bundle)
 		{
-			base.OnCreate (bundle);
+			base.OnCreate(bundle);
 
 			// Set our view from the "main" layout resource
-			SetContentView (Resource.Layout.Main);
+			SetContentView(Resource.Layout.Main);
 
-			mDrawerLayout = FindViewById<DrawerLayout> (Resource.Id.myDrawer);
-			mLeftDrawer = FindViewById<ListView> (Resource.Id.leftListView);
+			mDrawerLayout = FindViewById<DrawerLayout>(Resource.Id.myDrawer);
+			mLeftDrawer = FindViewById<ListView>(Resource.Id.leftListView);
 
-			mLeftItems.Add ("Unsere Schule");
-			mLeftItems.Add ("Aktuelles");
-			mLeftItems.Add ("Die SV");
-			mLeftItems.Add ("Termine");
-			mLeftItems.Add ("Klausuren");
-			mLeftItems.Add ("Kontakt");
-			mLeftItems.Add ("Einstellungen");
+			mLeftItems.Add("Unsere Schule");
+			mLeftItems.Add("Aktuelles");
+			mLeftItems.Add("Die SV");
+			mLeftItems.Add("Termine");
+			mLeftItems.Add("Klausuren");
+			mLeftItems.Add("Kontakt");
+			mLeftItems.Add("Einstellungen");
 
-			mDrawerToggle =	new MyActionBarDrawerToggle (this, mDrawerLayout, Resource.Drawable.ic_navigation_drawer, Resource.String.open_drawer, Resource.String.close_drawer);
-			mLeftAdapter = new ArrayAdapter (this, Android.Resource.Layout.SimpleListItem1, mLeftItems);
+			mDrawerToggle =	new MyActionBarDrawerToggle(this, mDrawerLayout, Resource.Drawable.ic_navigation_drawer, Resource.String.open_drawer, Resource.String.close_drawer);
+			mLeftAdapter = new ArrayAdapter(this, Android.Resource.Layout.SimpleListItem1, mLeftItems);
 			mLeftDrawer.Adapter = mLeftAdapter;
 
-			mLeftDrawer.ItemClick += (object sender, AdapterView.ItemClickEventArgs e) => {
+			mLeftDrawer.ItemClick += (object sender, AdapterView.ItemClickEventArgs e) =>
+			{
 				switch (mLeftDrawer.CheckedItemPosition)
 				{
 				//Unsere Schule
-				case 0:
-					Android.App.FragmentTransaction transaction = FragmentManager.BeginTransaction();
-					dialog_SignUp signUpDialog = new dialog_SignUp ();
-					signUpDialog.Show(transaction, "dialog fragment");
-					break;
+					case 0:
+						Android.App.FragmentTransaction transaction = FragmentManager.BeginTransaction();
+						dialog_SignUp signUpDialog = new dialog_SignUp();
+						signUpDialog.Show(transaction, "dialog fragment");
+						break;
 				//Aktuelles
-				case 1:
-					Android.App.FragmentTransaction aktuelles = FragmentManager.BeginTransaction();
-					aktuelles.Replace(Resource.Id.container, new Aktuelles_Fragment());
-					aktuelles.AddToBackStack(null);
-					aktuelles.Commit();
-					mDrawerLayout.CloseDrawer (mLeftDrawer);
-					ActionBar.Title ="Aktuelles";
-					break;
+					case 1:
+						Android.App.FragmentTransaction aktuelles = FragmentManager.BeginTransaction();
+						aktuelles.Replace(Resource.Id.container, new Aktuelles_Fragment());
+						aktuelles.AddToBackStack(null);
+						aktuelles.Commit();
+						mDrawerLayout.CloseDrawer(mLeftDrawer);
+						ActionBar.Title = "Aktuelles";
+						break;
 				//Die SV
-				case 2:
-					Android.App.FragmentTransaction diesv = FragmentManager.BeginTransaction();
-					diesv.Replace(Resource.Id.container, new diesv());
-					diesv.AddToBackStack(null);
-					diesv.Commit();
-					mDrawerLayout.CloseDrawer (mLeftDrawer);
-					ActionBar.Title = "Die SV";
-					break;
+					case 2:
+						Android.App.FragmentTransaction diesv = FragmentManager.BeginTransaction();
+						diesv.Replace(Resource.Id.container, new diesv());
+						diesv.AddToBackStack(null);
+						diesv.Commit();
+						mDrawerLayout.CloseDrawer(mLeftDrawer);
+						ActionBar.Title = "Die SV";
+						break;
 				//Termine
-				case 3:
-					Android.App.FragmentTransaction termine = FragmentManager.BeginTransaction();
-					termine.Replace(Resource.Id.container, new Termine_Fragment());
-					termine.AddToBackStack(null);
-					termine.Commit();
-					mDrawerLayout.CloseDrawer (mLeftDrawer);
-					break;
+					case 3:
+						Android.App.FragmentTransaction termine = FragmentManager.BeginTransaction();
+						termine.Replace(Resource.Id.container, new Termine_Fragment());
+						termine.AddToBackStack(null);
+						termine.Commit();
+						mDrawerLayout.CloseDrawer(mLeftDrawer);
+						break;
 				//Klausuren
-				case 4:
-					Android.App.FragmentTransaction klausuren = FragmentManager.BeginTransaction();
-					klausuren.Replace(Resource.Id.container, new Klausuren_Fragment());
-					klausuren.AddToBackStack(null);
-					klausuren.Commit();
-					mDrawerLayout.CloseDrawer (mLeftDrawer);
-					break;
+					case 4:
+						Android.App.FragmentTransaction klausuren = FragmentManager.BeginTransaction();
+						klausuren.Replace(Resource.Id.container, new Klausuren_Fragment());
+						klausuren.AddToBackStack(null);
+						klausuren.Commit();
+						mDrawerLayout.CloseDrawer(mLeftDrawer);
+						break;
 				//Kontakt
-				case 5:
-					Android.Widget.Toast.MakeText (this, "Hier kann man uns erreichen, bzw. sich beschweren.", Android.Widget.ToastLength.Short).Show();
-					break;
+					case 5:
+						Android.Widget.Toast.MakeText(this, "Hier kann man uns erreichen, bzw. sich beschweren.", Android.Widget.ToastLength.Short).Show();
+						break;
 				//Einstellungen
-				case 6:
-					Android.App.FragmentTransaction einstellungen = FragmentManager.BeginTransaction();
-					einstellungen.Replace(Resource.Id.container, new Klausuren_Fragment());
-					einstellungen.AddToBackStack(null);
-					einstellungen.Commit();
-					mDrawerLayout.CloseDrawer (mLeftDrawer);
-					break;
+					case 6:
+						Android.App.FragmentTransaction einstellungen = FragmentManager.BeginTransaction();
+						einstellungen.Replace(Resource.Id.container, new Klausuren_Fragment());
+						einstellungen.AddToBackStack(null);
+						einstellungen.Commit();
+						mDrawerLayout.CloseDrawer(mLeftDrawer);
+						break;
 				}
 
 					
 			};
 
 
-			mDrawerLayout.SetDrawerListener (mDrawerToggle);
-			ActionBar.SetDisplayHomeAsUpEnabled (true);
-			ActionBar.SetHomeButtonEnabled (true);
-			ActionBar.SetDisplayShowTitleEnabled (true);
+			mDrawerLayout.SetDrawerListener(mDrawerToggle);
+			ActionBar.SetDisplayHomeAsUpEnabled(true);
+			ActionBar.SetHomeButtonEnabled(true);
+			ActionBar.SetDisplayShowTitleEnabled(true);
 
-            try
-            {
-                var reader = new RSSReader("http://fg-kassel.de/feed/index.php", this);
-                reader.DownloadFeed("fg_feed.rss");
-                var rss = new RSSFeed();
-                rss.Load(Path.Combine(RSSReader.FeedFolder, "fg_feed.rss"));
-                // new AlertDialog.Builder(this).SetMessage (((RSSFeedArticle)rss.Articles[0]).Content).SetTitle(rss.Title).Show();
-
-                List<RSSFeedArticle> artikels = rss.Articles.Select(feed => (RSSFeedArticle)feed).ToList();
-            }
-            catch
-            {
-                Android.Widget.Toast.MakeText (this, "RSS-Feed konnte nicht geladen werden.", Android.Widget.ToastLength.Long).Show();
-            }
+			try
+			{
+				var reader = new RSSReader("http://fg-kassel.de/feed/index.php", this);
+				reader.DownloadFeed("fg_feed.rss");
+			}
+			catch
+			{
+				Android.Widget.Toast.MakeText(this, "RSS-Feed konnte nicht geladen werden.", Android.Widget.ToastLength.Long).Show();
+			}
 
 		}
 
 
 
-		protected override void OnPostCreate (Bundle savedInstanceState)
+		protected override void OnPostCreate(Bundle savedInstanceState)
 		{
-			base.OnPostCreate (savedInstanceState);
-			mDrawerToggle.SyncState ();
+			base.OnPostCreate(savedInstanceState);
+			mDrawerToggle.SyncState();
 
 		}
 
-		public override void OnConfigurationChanged (Android.Content.Res.Configuration newConfig)
+		public override void OnConfigurationChanged(Android.Content.Res.Configuration newConfig)
 		{
-			base.OnConfigurationChanged (newConfig);
-			mDrawerToggle.OnConfigurationChanged (newConfig);
+			base.OnConfigurationChanged(newConfig);
+			mDrawerToggle.OnConfigurationChanged(newConfig);
 		}
-		public override bool OnOptionsItemSelected (IMenuItem item)
+
+		public override bool OnOptionsItemSelected(IMenuItem item)
 		{
 
-			if (mDrawerToggle.OnOptionsItemSelected (item)) 
+			if (mDrawerToggle.OnOptionsItemSelected(item))
 			{
 				return true;
 			}
 
 		
-			return base.OnOptionsItemSelected (item);
+			return base.OnOptionsItemSelected(item);
 
 
 		
--- FG_App/FG_App/RSSReader.cs
+++ FG_App/FG_App/RSSReader.cs
@@ -16,29 +16,30 @@ namespace FG_App
 	public class RSSReader
 	{
 		string Url { get; set; }
-        Activity CurrentActivity { get; set; }
+
+		Activity CurrentActivity { get; set; }
 
 		private Func<string, bool> IsNullOrWhiteSpaceOrEmpty = s => 
 			String.IsNullOrEmpty(s) || String.IsNullOrWhiteSpace(s);
 
-        public readonly static string FeedFolder = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+		public readonly static string FeedFolder = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
 
 		public string MakeFileName()
 		{
-			return string.Format (
+			return string.Format(
 				"{0}.rss", 
-                String.Concat(
-                    new Uri (this.Url)
-    					.GetLeftPart (UriPartial.Authority)
-    					.SkipWhile (c => c != '.')
-    					.Skip (1)
-                        .TakeWhile (c => c != '.')
-                ));
+				String.Concat(
+					new Uri(this.Url)
+    					.GetLeftPart(UriPartial.Authority)
+    					.SkipWhile(c => c != '.')
+    					.Skip(1)
+                        .TakeWhile(c => c != '.')
+				));
 		}
 
-        public void DownloadFeed(string fileName)
-        {
-            /* try{
+		public void DownloadFeed(string fileName)
+		{
+			/* try{
                 using (var client = new WebClient ()) {
                     // client.DownloadProgressChanged  += new DownloadProgressChangedEventHandler(null);
                     // client.DownloadFileCompleted    += new AsyncCompletedEventHandler(null);
@@ -48,25 +49,32 @@ namespace FG_App
             catch (UriFormatException) {
                 new AlertDialog.Builder(CurrentActivity).SetMessage ("URL'is forma mala est ").SetTitle("System.UriFormatException").Show();
             } */
-            var client = new WebClient();
-            client.DownloadStringCompleted += (s, e) => {
-                var text = e.Result; // get the downloaded text
-                File.WriteAllText (Path.Combine (FeedFolder, fileName), text); // writes to local storage   
-            };
-            var url = new Uri(this.Url);
-            client.Encoding = Encoding.UTF8;
-            client.DownloadStringAsync(url);
-        }
+			var client = new WebClient();
+			client.DownloadStringCompleted += (s, e) =>
+			{
+				var text = e.Result; // get the downloaded text
+				File.WriteAllText(Path.Combine(FeedFolder, fileName), text); // writes to local storage   
+
+				var rss = new RSSFeed();
+				rss.Load(Path.Combine(RSSReader.FeedFolder, "fg_feed.rss"));
+				// new AlertDialog.Builder(this).SetMessage (((RSSFeedArticle)rss.Articles[0]).Content).SetTitle(rss.Title).Show();
+
+				List<RSSFeedArticle> artikels = rss.Articles.Select(feed => (RSSFeedArticle)feed).ToList();
+			};
+			var url = new Uri(this.Url);
+			client.Encoding = Encoding.UTF8;
+			client.DownloadStringAsync(url);
+		}
 
 		public void DownloadFeed()
 		{
-            DownloadFeed("downloaded_feed.rss");		    
+			DownloadFeed("downloaded_feed.rss");		    
 		}
 
-        public RSSReader (string url, Activity activity)
+		public RSSReader(string url, Activity activity)
 		{
 			Url = url;
-            CurrentActivity = activity;
+			CurrentActivity = activity;
 		}
 	}
 }
