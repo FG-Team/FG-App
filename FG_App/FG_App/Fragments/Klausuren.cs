a7ae953ca4ef48c8946bbbc647bdda057c283064
derrichter1 <der_richter@outlook.com>
2015-05-16 22:18:38 +0200

    Klausuren Seite mit funktionierenden Click Events (im Application Output zu sehen)

--- FG_App/FG_App/Fragments/Klausuren.cs
+++ FG_App/FG_App/Fragments/Klausuren.cs
@@ -16,8 +16,11 @@ namespace FG_App
 	/// <summary>
 	/// Initialisiert eine neue Instanz der Klausuren Klasse.
 	/// </summary>
-	public class Klausuren : Fragment
+    public class Klausuren : Fragment
 	{
+        private Button mBtnKlausurenE2;
+        private Button mBtnKlausurenQ2;
+        private Button mBtnKlausurenQ4;
 		/// <summary>
 		/// Übersteuerung für OnCreateView.
 		/// </summary>
@@ -26,9 +29,28 @@ namespace FG_App
 		/// <param name="savedInstanceState">Das Bundle.</param>
 		public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
 		{
-			View klausuren = inflater.Inflate(Resource.Layout.Klausuren, container, false);
-			return klausuren;
+            base.OnCreateView(inflater, container, savedInstanceState);
+
+            var view = inflater.Inflate(Resource.Layout.Klausuren, container, false);
+
+            mBtnKlausurenE2 = view.FindViewById<Button>(Resource.Id.KlausurenE2Btn);
+            mBtnKlausurenQ2 = view.FindViewById<Button>(Resource.Id.KlausurenQ2Btn);
+            mBtnKlausurenQ4 = view.FindViewById<Button>(Resource.Id.KlausurenQ4Btn);
+
+            mBtnKlausurenE2.Click += (object sender, EventArgs e) => 
+                Console.WriteLine("Hello");
+
+            mBtnKlausurenQ2.Click += (object sender, EventArgs e) => 
+                Console.WriteLine("Q2");
+
+            mBtnKlausurenQ4.Click += (object sender, EventArgs e) => 
+                Console.WriteLine("Q4");
+
+            return view;
 		}
+
+
+
 	}
 }
 
--- FG_App/FG_App/Resources/Resource.designer.cs
+++ FG_App/FG_App/Resources/Resource.designer.cs
@@ -75,38 +75,47 @@ namespace FG_App
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f060004
-			public const int container = 2131099652;
-			
-			// aapt resource value: 0x7f06000b
-			public const int diesv = 2131099659;
-			
-			// aapt resource value: 0x7f060008
-			public const int fragment_container = 2131099656;
-			
-			// aapt resource value: 0x7f060009
-			public const int horizontalScrollView1 = 2131099657;
-			
-			// aapt resource value: 0x7f060006
-			public const int imageUnsereSchule = 2131099654;
-			
-			// aapt resource value: 0x7f06000a
-			public const int imageView2 = 2131099658;
-			
-			// aapt resource value: 0x7f060005
-			public const int leftListView = 2131099653;
-			
 			// aapt resource value: 0x7f060002
-			public const int myDrawer = 2131099650;
+			public const int KlausurenE2Btn = 2131099650;
 			
 			// aapt resource value: 0x7f060003
-			public const int myListView = 2131099651;
+			public const int KlausurenQ2Btn = 2131099651;
+			
+			// aapt resource value: 0x7f060004
+			public const int KlausurenQ4Btn = 2131099652;
+			
+			// aapt resource value: 0x7f060007
+			public const int container = 2131099655;
+			
+			// aapt resource value: 0x7f06000e
+			public const int diesv = 2131099662;
+			
+			// aapt resource value: 0x7f06000b
+			public const int fragment_container = 2131099659;
+			
+			// aapt resource value: 0x7f06000c
+			public const int horizontalScrollView1 = 2131099660;
+			
+			// aapt resource value: 0x7f060009
+			public const int imageUnsereSchule = 2131099657;
+			
+			// aapt resource value: 0x7f06000d
+			public const int imageView2 = 2131099661;
+			
+			// aapt resource value: 0x7f060008
+			public const int leftListView = 2131099656;
+			
+			// aapt resource value: 0x7f060005
+			public const int myDrawer = 2131099653;
+			
+			// aapt resource value: 0x7f060006
+			public const int myListView = 2131099654;
 			
 			// aapt resource value: 0x7f060000
 			public const int myListViewOld = 2131099648;
 			
-			// aapt resource value: 0x7f060007
-			public const int scrollView1 = 2131099655;
+			// aapt resource value: 0x7f06000a
+			public const int scrollView1 = 2131099658;
 			
 			// aapt resource value: 0x7f060001
 			public const int textView1 = 2131099649;
